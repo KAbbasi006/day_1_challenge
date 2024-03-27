5de82ccd9f1f58b94b2011c9a4626db3192b343b
KAbbasi006 <abbasikomal6@gmail.com>
2024-03-28 00:38:36 +0500

    assignment Q1, Q2 and Q3

---
+++ main.ts
@@ -0,0 +1,18 @@
+
+//Q1. Installation
+//Ans: Installation completed
+
+
+//Q2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some TypeScript today?”
+let person:string = "Komal";
+console.log(`Hello ${person}, would you like to learn some TypeScript today?`);
+
+
+//Q3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName:string = "Komal";
+//Print name in all small letters
+console.log("In LowerCase: " + personName.toLowerCase());
+//Print name in all Capital letters
+console.log("In UpperCase: " + personName.toUpperCase());
+//Print First letter of each word in Capital letter
+console.log("In TitleCase: " + personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
